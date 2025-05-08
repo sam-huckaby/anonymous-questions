c9e22e3f558844c175ed95b075a1dcdcd109fca4
Sam Huckaby <samhuckaby@gmail.com>
2025-05-08 08:34:07 -0400

    Add ability to configure the site title using environment variables

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container flex justify-center items-center">
         <div className="flex items-center gap-2">
           <MessageCircleIcon className="h-6 w-6 text-primary" />
-          <h1 className="text-xl font-bold tracking-tight">Free Questions</h1>
+          <h1 className="text-xl font-bold tracking-tight">{process.env.SITE_TITLE}</h1>
         </div>
       </div>
     </header>
